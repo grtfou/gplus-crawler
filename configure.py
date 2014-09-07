cdebb7c927430942737e5bf4841a0ed622816b84
grtfou <lightgdi@gmail.com>
2014-09-07 23:11:30 +0800

    Edit: UI message

--- configure.py
+++ configure.py
@@ -2,10 +2,12 @@
 VERSION = '0.4.0'
 UI_TITLE = u"Google plus+ 相片+影片備份"
 
-about_txt = [u"一個備份Google+ 訊息裡相片+影片的小玩具",
+about_txt = (u"一個備份Google+ 訊息裡相片+影片的小玩具",
              u" \n版本: {0}".format(VERSION),
-             u" Enhanced: 加速 (改變抓取方式)"
-            ]
+             u" \n最後修改: 2014.09.07",
+             u" \nEnhanced: 加速 (改變抓取方式)",
+             u" \n待修: 只能抓1000則訊息內",
+            )
 
 MENU_ABOUT_TXT = '\n'.join(about_txt)
 MENU_ABOUT_TITLE = u"版本資訊"
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ copyDependentFiles=True
 silent = True
 # includes = ["lxml", "lxml._elementpath", "lxml.etree",
 #             "gzip", "encodings.utf_8", "encodings.ascii"]
-includes = ["gzip", "encodings.utf_8", "encodings.ascii"]
+includes = ["encodings.utf_8", "encodings.ascii"]
 
 setup(name='gplus_crawler',
      version = configure.VERSION,
